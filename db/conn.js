77c74fa6b7a7a118db2fa30a22b7e008c9c2a06d
Ir786921 <imran.raza.cs24@iilmcet.ac.in>
2023-11-02 07:37:00 +0530

    new way of connecting to mongodb

--- db/conn.js
+++ db/conn.js
@@ -1,13 +1,40 @@
 const mongoose = require("mongoose");
-require('dotenv').config();
-mongoose.connect(process.env.CONNECTION_URL,{
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-}).then(()=>{
-    console.log("connection established");
-}).catch((err)=>{
-    console.log("no connection");
-})
+// require('dotenv').config();
+// mongoose.connect(process.env.CONNECTION_URL,{
+//     useNewUrlParser: true, 
+//     useUnifiedTopology: true
+// }).then(()=>{
+//     console.log("connection established");
+// }).catch((err)=>{
+//     console.log("no connection");
+// })
+
+const connectDB = async (uri) => {
+    try {
+      await mongoose.connect(uri, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      });
+      console.log("Connected to MongoDB");
+    } catch (error) {
+      console.error("Failed to connect to MongoDB:", error);
+    }
+  };
+
+
+
+// async function connect(uri){
+//     console.log("flow come here");
+//     try {
+//         await mongoose.connect(uri);
+//         console.log("connected to MongoDB");
+//     }
+//     catch(error) {
+//         console.log("not connected to mongoDB",error);
+//     }
+// }
+
+
 
 // const mongoString = process.env.CONNECTION_URL
 
@@ -38,7 +65,7 @@ mongoose.connect(process.env.CONNECTION_URL,{
 //     }
 // };
 
-// module.exports = connectDB;
+module.exports = connectDB;
 
 
 
--- index.js
+++ index.js
@@ -1,35 +1,71 @@
 // require("dotenv").config();
-const express = require("express");
+// const express = require("express");
 
-const Cors = require("cors");
-require("./db/conn");
-const app = express();
-app.use(express.json());
-app.use(Cors());
-const port = process.env.PORT || 3000;
-const Route = require("./Routes/router");
-// const connectDB = require("./db/conn")
-app.use("/api",Route);
+// const Cors = require("cors");
+// require("./db/conn");
+// const app = express();
+// app.use(express.json());
+// app.use(Cors());
+// const port = process.env.PORT || 3000;
+// const Route = require("./Routes/router");
+// const connect = require("./db/conn")
+// app.use("/api",Route);
 
-app.get("/",(req,res)=>{
-    res.send("Server is started");
-})
+// app.get("/",(req,res)=>{
+//     res.send("Server is started");
+// })
 
 // const start = async ()=>{
 //     try {
-//         await connectDB(process.env.CONNECTION_URL)
-//         app.listen(port,()=>{
-//             console.log( `server is running in http://localhost:${port}`);
-//         })
+//         await connect(process.env.CONNECTION_URL)
+      
         
 //     } catch (error) {
 //         console.log(error);
 //     }
 // }
 // start();
-app.listen(port,()=>{
-    console.log( `server is running in http://localhost:${port}`);
-})
+
+// app.listen(port,()=>{
+//     console.log( `server is running in http://localhost:${port}`);
+// })
+
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose"); // Import Mongoose
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+const port = process.env.PORT || 3000;
+const Route = require("./Routes/router");
+app.use("/api", Route);
+
+// Connect to MongoDB
+const connectDB = async () => {
+    try {
+      await mongoose.connect(process.env.CONNECTION_URL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      });
+      console.log("Connected to MongoDB");
+    } catch (error) {
+      console.error("Failed to connect to MongoDB:", error);
+    }
+  };
+  
+  connectDB();// Call the connectDB function to establish the MongoDB connection
+
+app.get("/", (req, res) => {
+  res.send("Server is started");
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
+
 
 
    
